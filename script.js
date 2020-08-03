496d112dfb2d735f8cc568c0ddf42584dddba420
Julia <yulyakonoval1998@gmail.com>
2020-08-03 15:18:12 +0300

    random colors paint

--- script.js
+++ script.js
@@ -2,14 +2,40 @@
 const container = document.createElement("div");
 container.className = "gridContainer";
 
+const buttonContainer = document.createElement("div");
+buttonContainer.className = "buttonContainer";
+
+const randomColorBtn = document.createElement("button");
+randomColorBtn.textContent = "Random color";
+buttonContainer.appendChild(randomColorBtn);
+
+const purpleBtn = document.createElement("button");
+purpleBtn.textContent = "Select purple";
+buttonContainer.appendChild(purpleBtn);
+
+const blackBtn = document.createElement("button");
+blackBtn.textContent = "Select black";
+buttonContainer.appendChild(blackBtn);
+
 function createGrid(rows, cols) {
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
 
   for (i = 0; i < rows * cols; i++) {
     let cell = document.createElement("div");
-    cell.innerText = i + 1;
+
     container.appendChild(cell).className = "cell";
+
+    randomColorBtn.addEventListener("click", function () {
+      cell.onmouseover = function () {
+        var x = Math.floor(Math.random() * 256);
+        var y = Math.floor(Math.random() * 256);
+        var z = Math.floor(Math.random() * 256);
+        let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+        cell.style.background = bgColor;
+        // cell.classList.add("hover");
+      };
+    });
   }
 }
 createGrid(16, 16);
